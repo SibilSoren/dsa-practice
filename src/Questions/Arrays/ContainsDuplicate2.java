99d9c4d9a89463ba9909621677ed9032ee8c17a5
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2025-10-15 23:14:16 +0530

    ContainsDuplicate2 problem solved

---
+++ src/Questions/Arrays/ContainsDuplicate2.java
@@ -0,0 +1,23 @@
+package Questions.Arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate2 {
+    static void main() {
+
+    }
+
+    static boolean containsDuplicate(int[] nums, int k){
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0 ;  i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
